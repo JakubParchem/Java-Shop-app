e8b2ac33c394d7b06c57bb42781705b4b41a59a2
JakubParchem <jakubparchem@gmail.com>
2024-06-23 00:23:17 +0200

    small changes

--- src/Product.java
+++ src/Product.java
@@ -1,14 +1,19 @@
 public class Product {
-    private String name;
-    private double price;
+    protected String name;
+    protected double price;
 
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
-
     @Override
     public String toString() {
-        return name + "    " + price;
+        return name + "    cena: " + price+" zł/szt";
+    }
+    public void updatePrice(double nprice){
+        price=nprice;
+    }
+    public void changeName(String nname){
+        name=nname;
     }
 }
