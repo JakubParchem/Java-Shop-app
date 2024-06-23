02758b883e3f3a5e3763117ff8be74901fb57184
JakubParchem <jakubparchem@gmail.com>
2024-06-23 22:27:59 +0200

    added Storage Menagment

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Storage storage=new Storage();
-        storage.display();
-        storage.edit_product(0);
-        storage.display();
+        storage.Storage_Menagment();
     }
 }
--- src/Storage.java
+++ src/Storage.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -31,19 +30,23 @@ public class Storage{
     public void edit_product(int i) throws IOException {
         while (true){
             System.out.println("Editing :"+products.get(i));
-            System.out.print("[0 : price, 1 : name] :  ");
+            System.out.print("0 - price\n 1 - name\nwhich do you want to edit : ");
             switch (input.nextInt()){
                 case 0:{
-                    System.out.print("enter new price: ");
+                    System.out.print("\nenter new price: ");
                     products.get(i).updatePrice(input.nextDouble());
                     save();
                     return;
                 }
                 case 1:{
-                    System.out.print("Enter new name: ");
-                    products.get(i).changeName(input.next());
+                    System.out.print("\nEnter new name: ");
+                    input.nextLine();
+                    products.get(i).changeName(input.nextLine());
                     save();
                     return;
+                }
+                default:{
+                    System.out.println("wrong input");
                 }
             }
         }
@@ -59,5 +62,61 @@ public class Storage{
             }
         }
         writer.close();
+    }
+    public void Storage_Menagment() throws IOException {
+        int i=0;
+        String s="";
+        while(true){
+            display();
+            System.out.println("------------------------\nStorage Menagment\n\n0 - edit\n1 - add\n2 - remove\n3 - exit\n");
+            System.out.print("what do you want to do: ");
+            switch (input.nextInt()){
+                case 0:{
+                    while (true){
+                        display();
+                        System.out.print("------------------------\nwhich product do you want to edit[id] : ");
+                        i=input.nextInt();
+                        if(i>=0 && i< products.size()){
+                            edit_product(i);
+                            break;
+                        }
+                        else {
+                            System.out.println("wrong input");
+                        }
+                    }
+                    break;
+                }
+                case 1:{
+                    System.out.print("input name: ");
+                    input.nextLine();
+                    s=input.nextLine();
+                    System.out.print("input price: ");
+                    addProduct(new Product(s,input.nextDouble()));
+                    break;
+                }
+                case 2:{
+                    while (true){
+                        display();
+                        System.out.print("------------------------\nwhich product do you want to delete[id] : ");
+                        i=input.nextInt();
+                        if(i>=0 && i< products.size()){
+                            removeproduct(i);
+                            break;
+                        }
+                        else {
+                            System.out.println("wrong input");
+                        }
+                    }
+                    break;
+                }
+                case 3:{
+                    return;
+                }
+                default:{
+                    System.out.println("wrong input");
+                    break;
+                }
+            }
+        }
     }
 }
